d1c9f9a5d8e612accad343b8ce10d0845a128429
Mehraj Hosen <meherajhosen786@gmail.com>
2025-06-16 14:06:49 +0600

    compo add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/navbar/Navbar'
+import Banner from './components/banner/Banner'
+import About from './components/about/About'
+import Testimonial from './components/testimonial/Testimonial'
+import Feature from './components/feature/Feature'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 function App() {
 
   return (
 <>
-Hello World
+<Navbar/>
+<Banner/>
+<About/>
+<Testimonial/>
+<Feature/>
+<Contact/>
+<Footer/>
 </>
   )
 }
